88f6eb7a1bf51f7eed36c9bd7d418552ef8236c1
Simone Trenta <trenta.1724141@studenti.uniroma1.it>
2019-01-27 14:20:26 +0100

    myProcess

---
+++ myProcess/README.md
@@ -0,0 +1,5 @@
+Qui svilupperò del codice per simulare una coda di processi, che verranno letti
+da file, processati (processamento simulato) e terminati.
+
+Non ci sarà ancora lo scheduler, ma unicamente una lista di processi che vengono
+processati. 
---
+++ myProcess/myProcess.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "linked_list.h"
+
+typedef enum {CPU=0, IO=1} TypeResource; 
+//tipo enum per specificare il tipo di risorsa che il processo utilizza
+
+typedef struct {
+	//evento del processo
+	ListItem list;
+	TypeResource type;
+	int duration;
+} my_event;
+
+typedef struct {
+	//struttura che simula un processo
+	ListItem list;
+	int pid; //assegnato dal programma di prova
+	int arrival_time;
+	ListHead events;
+} my_process;
